779b7675df413b4f6ef69b0c43fa5ea7cef35864
Blessing Philips <blessingadesina@gmail.com>
2018-01-21 19:47:11 +0100

    project setup

---
+++ README.md
@@ -0,0 +1,26 @@
+# naija-state-local-government
+
+A simple utility library that lists Nigeria states and local governments
+
+### Installation
+
+``` Javascript
+
+npm install naija-state-local-government
+
+```
+
+### Example
+
+``` Javascript
+
+var NaijaStates = require('naija-state-local-government');
+console.log(NaijaStates.all);
+console.log(NaijaStates.states());
+console.log(NaijaStates.lgas("Oyo"))
+
+```
+
+### Features
+
+List all local government areas and state in Nigeria
---
+++ index.js
@@ -0,0 +1,28 @@
+var statesAndLocalGov = require('./src/statesAndLocalGov.json')
+
+module.exports = {
+    all: statesAndLocalGov,
+    states: function () {
+        var naijaStates = [];
+        statesAndLocalGov.map(function (nigeriaStates){
+            return naijaStates.push(nigeriaStates.state)
+        })
+        return naijaStates;
+    },
+    lgas: function (state) {
+        state = state.toLowerCase().trim();
+        if(!state || state == ""){
+            throw new Error('Invalid Nigeria State');
+        }
+        if (state === 'fct' || state === 'f.c.t' || state === 'abuja' || state === 'f c t'){
+            state = 'Federal Capital Territory'
+        }
+        var stateLocalGov;
+        statesAndLocalGov.map(function (nigeriaStates) {
+            if (nigeriaStates.state.toLowerCase() === state.toLowerCase().trim()){
+            return stateLocalGov = nigeriaStates.lgas;
+            }
+        })
+        return stateLocalGov;
+    }
+  };
