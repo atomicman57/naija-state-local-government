e5fbda3f68199d46fdf863791376856976a6611d
seyi-adeleke <adetokunbo.adeleke@andela.com>
2018-01-22 15:13:24 +0100

    feature(lga's): refactor lga function
    Instead of using the Map function to iterate over the states
    Array.find() should be used as find() terminates when the value is found
    over a map which iterates over all the objects in the array.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var statesAndLocalGov = require('./src/statesAndLocalGov.json')
+var statesAndLocalGov = require('./src/statesAndLocalGov.json');
 
 module.exports = {
     all: function() {
@@ -8,23 +8,25 @@ module.exports = {
         var naijaStates = [];
         statesAndLocalGov.map(function (nigeriaStates){
             return naijaStates.push(nigeriaStates.state)
-        })
+        });
         return naijaStates;
     },
     lgas: function (state) {
-        state = state.toLowerCase().trim();
-        if(!state || state == ""){
+        var stateLocalGov;
+
+        if(!state || state.toLowerCase().trim() === ''){
             throw new Error('Invalid Nigeria State');
         }
+        state = state.toLowerCase().trim();
+
         if (state === 'fct' || state === 'f.c.t' || state === 'abuja' || state === 'f c t'){
             state = 'Federal Capital Territory'
         }
-        var stateLocalGov;
-        statesAndLocalGov.map(function (nigeriaStates) {
+        statesAndLocalGov.find(function (nigeriaStates) {
             if (nigeriaStates.state.toLowerCase() === state.toLowerCase().trim()){
-            return stateLocalGov = nigeriaStates.lgas;
+                return stateLocalGov = nigeriaStates.lgas;
             }
-        })
+        });
         return stateLocalGov;
     }
-  };
+};
