19e8ec63cd93343633cf684db4164ed934b28b69
Restyled.io <commits@restyled.io>
2021-12-19 02:34:19 +0000

    Restyled by prettier

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
-var statesAndLocalGov = require('./src/statesAndLocalGov.json');
+var statesAndLocalGov = require("./src/statesAndLocalGov.json");
 
 function _lower(input) {
   if (input) {
-    return input.toLowerCase().trim()
+    return input.toLowerCase().trim();
   }
-  return ''
+  return "";
 }
 
 function isFCT(state) {
-  if ([ 'fct', 'f.c.t', 'abuja', 'f c t' ].includes(state)) {
-    return 'Federal Capital Territory'
+  if (["fct", "f.c.t", "abuja", "f c t"].includes(state)) {
+    return "Federal Capital Territory";
   }
-  return state
+  return state;
 }
 
 var NaijaStates = {
-  all : function() { return statesAndLocalGov; },
-  states : function() {
-    return statesAndLocalGov.map(function(
-        nigeriaStates) { return nigeriaStates.state; });
+  all: function () {
+    return statesAndLocalGov;
   },
-  senatorial_districts : function(state) {
-    var response = statesAndLocalGov.find(function(nigeriaStates) {
+  states: function () {
+    return statesAndLocalGov.map(function (nigeriaStates) {
+      return nigeriaStates.state;
+    });
+  },
+  senatorial_districts: function (state) {
+    var response = statesAndLocalGov.find(function (nigeriaStates) {
       return _lower(nigeriaStates.state) === _lower(isFCT(state));
     });
-    if (response)
-      return response.senatorial_districts;
-    return []
+    if (response) return response.senatorial_districts;
+    return [];
   },
-  lgas : function(state) {
-    var response = statesAndLocalGov.find(function(nigeriaStates) {
+  lgas: function (state) {
+    var response = statesAndLocalGov.find(function (nigeriaStates) {
       return _lower(nigeriaStates.state) === _lower(isFCT(state));
     });
-    if (response)
-      return response;
-    return { state: "", senatorial_districts: [], lgas: [] }
-  }
+    if (response) return response;
+    return { state: "", senatorial_districts: [], lgas: [] };
+  },
 };
 
-module.exports = NaijaStates
+module.exports = NaijaStates;
