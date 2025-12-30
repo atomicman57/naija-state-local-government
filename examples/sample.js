86ed28a74e9b280a11f12a1fcd2773c78fecac21
Philips Blessing <philips@example.com>
2025-12-30 15:46:13 +0100

    feat: enhance sample.js with additional functionality
    - Update variable declaration to use const for NaijaStates
    - Add console logs for states, LGAs, and senatorial districts of Oyo

--- examples/sample.js
+++ examples/sample.js
@@ -1,3 +1,7 @@
-var NaijaStates = require('../');
+const NaijaStates = require('../');
 console.log(NaijaStates.all());
 
+
+console.log(NaijaStates.states());
+console.log(NaijaStates.lgas("oyo"));
+console.log(NaijaStates.senatorial_districts("oyo"));
